6dbba8ff67e2b57c398fc97aeae2db80b06a3d50
Willian Mitsuda <wmitsuda@gmail.com>
2017-10-08 21:23:31 -0300

    Bug fix: zeppelin-solidity 1.3.0 changed the Crowdsale contract to use timestamps instead of block numbers (see https://github.com/OpenZeppelin/zeppelin-solidity/issues/350)

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 var DemoCoinCrowdsale = artifacts.require("./DemoCoinCrowdsale.sol")
 
 module.exports = function(deployer, network, accounts) {
-  const startBlock = web3.eth.blockNumber + 2
-  const endBlock = startBlock + 300
+  const startTime = web3.eth.getBlock('latest').timestamp // Start immediately
+  const endTime = startTime + 7 * 24 * 60 * 60 // 7 days
   const rate = new web3.BigNumber(1000)
   const wallet = web3.eth.accounts[0]
 
-  deployer.deploy(DemoCoinCrowdsale, startBlock, endBlock, rate, wallet)
+  deployer.deploy(DemoCoinCrowdsale, startTime, endTime, rate, wallet)
 };
